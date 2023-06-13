42e8f42b767ed7f21fec7939642d76bde9daa17f
薛运成 <datouguix@gmail.com>
2023-06-13 20:23:31 +0800

    feat: add 001

---
+++ 001/001.go
@@ -0,0 +1,13 @@
+package l001
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+
+	return nil
+}
